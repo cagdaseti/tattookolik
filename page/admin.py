ab8812a555cd36c50999e0494e98b0cc82f09eba
cagdaseti <cagdaseti@outlook.com>
2022-02-09 23:03:29 +0300

    slug media statik admin

--- page/admin.py
+++ page/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 from.models import Page
 
+class PageAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug":("title",)}
+    list_display = (
+        'pk',
+        'title',
+        'status',
+        'updated_at',
+        )
+    list_filter = ('status',)
+    list_editable = (
+        'title',
+        'status',
+        )
 
-admin.site.register(Page)
+
+admin.site.register(Page,PageAdmin)
+
--- page/models.py
+++ page/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 STATUS = [
+    # * sol taraf db yazan ismi, sağ taraf UI da görünün ismi
     ('draft','Taslak'),
     ('published','Yayinlandi'),
     ('deleted','Silindi'),
@@ -11,9 +12,16 @@ STATUS = [
 
 class Page(models.Model):
     title = models.CharField(max_length=200)
-    # slug
+    slug = models.SlugField(
+        max_length=200,
+        default="",
+        )
     content = models.TextField()
-    cover_image = models.ImageField(upload_to='page')
+    cover_image = models.ImageField(
+        upload_to='page',
+        null=True,
+        blank=True,
+        )
     status = models.CharField(
         default="draft",
         choices=STATUS,
